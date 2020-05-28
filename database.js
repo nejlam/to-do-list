25f34090bbdbde9861b907a31dedca72db66b069
Nejla Maksumic <nejla.maksumic@gmail.com>
2020-05-28 17:15:59 +0200

    ClearDB setup

--- database.js
+++ database.js
@@ -1,10 +1,22 @@
 const mysql = require('mysql2');
-const dbConnection = mysql
+
+/*const dbConnection = mysql
 	.createPool({
 		host: 'localhost',
 		user: 'root',
-		password: 'password',
+		password: '',
 		database: 'todo_list'
 	})
 	.promise();
+*/
+
+const dbConnection = mysql
+	.createPool({
+		host: 'b680eced3b8410',
+		user: 'eu-cdbr-west-03.cleardb.net',
+		password: '1c5d0a48',
+		database: 'heroku_c716d639849d18e'
+	})
+	.promise();
+
 module.exports = dbConnection;
